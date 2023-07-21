45bb5242df042302884fe1de8699a5c749ac51ab
Fiterman <gfiterman96@gmail.com>
2023-07-21 14:05:21 -0300

    Initial commit: intall Vue 3 with Vite install sass and initialize variables install and import bootstrap clear default vue3+vite project
    LET's do it!

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <main class="container">
+    <div class="row text-center">
+      <div class="col">
+        <h1>Home View</h1>
+      </div>
+    </div>
+    <div class="row text-center ">
+      <div class="col">
+        <p>olá</p>
+      </div>
+      <div class="col">
+        <p>bootstrap</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.col {
+  background-color: $color-highlight;
+  outline: $color-text-inverted 2px solid;
+  color: $color-text-inverted;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          $color-background: #FAFAFC;
+          $color-borders-light: #E6EAF2;
+          $color-borders-main: rgba(81, 93, 116, 0.50);
+          $color-elevate: #10C300;
+          $color-highlight: #F30168 ;
+          $color-text-inverted: #fff;
+          $color-text-secondary: #666;
+          $color-text: #292D32;
+          $font-family: 'Sora', Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+          $font-size-mainTitle: 28px;
+          $font-size-secondaryTitle: 26px;
+          $font-size-subtitle: 18px;
+          $font-size-default: 17px;
+          $font-size-xm: 14px;
+          $font-size-sm: 16px;
+        `
+      }
+    }
+  }
+})
