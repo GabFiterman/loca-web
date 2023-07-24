57369b0a97988ed01d672e1aaebd7e5c49448c78
Fiterman <gfiterman96@gmail.com>
2023-07-23 22:37:01 -0300

    testing: directly associate JSON

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,24 +1,15 @@
-import { defineStore } from 'pinia'
-import axios from 'axios'
+import { defineStore } from 'pinia' 
+import textData from '@/data/locaWeb_textData.json'
 
 export const useStore = defineStore('store', {
   state: () => ({
-    jsonData: null,
+    jsonData: textData,
     isUserLogged: false,
     userToken: null,
     username: null,
     choosedPlan: null
   }),
   actions: {
-    async fetchJsonData() {
-      try {
-        const response = await axios.get('/src/data/locaWeb_textData.json')
-        this.jsonData = response.data
-      } catch (error) {
-        console.error('Erro ao buscar os dados do JSON:', error)
-        throw error
-      }
-    },
     setUserLogin(state, token, username) {
       this.isUserLogged = state
       this.userToken = token
